c1b4214c98c5e97e360d02ed6a949585d8b1a617
Mayur <mayurzope555@gmail.com>
2020-09-28 12:58:57 +0530

    [Added]: Validating the Pin Code using the Regex

--- Pin_Code.java
+++ Pin_Code.java
@@ -1,10 +1,16 @@
 /* Welcome to the PIN Code Matching Problem */
 
+import java.util.regex.*;
+
 public class Pin_Code {
-
 	public static void main(String[] args) {
-		System.out.println("Welcome to the Pin Code Matching Problem");
-
+		System.out.println("Welcome to the Pin Code Problem");
+		
+		boolean matcher = "400088".matches("[\\d ]{6}");
+		if(matcher == true) {
+			System.out.println("Valid PIN Code");
+		}else {
+			System.out.println("Invalid PIN Code");
+		}
 	}
-
 }
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # PIN-Code-Matching-Problem
+
+In this Use Case 1 we are matching the PIN Code with the given PinCode
+if satisfies it gets validated else Invalid.
