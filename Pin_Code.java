da93a39002d90d548ad087b7a74d623e9b783f97
Mayur <mayurzope555@gmail.com>
2020-09-28 20:40:47 +0530

    [Mayur]Added: Pin Code problem restricting beginning alphabet

--- Pin_Code.java
+++ Pin_Code.java
@@ -1,12 +1,18 @@
 /* Welcome to the PIN Code Matching Problem */
 
 import java.util.regex.*;
+import java.util.*;
 
 public class Pin_Code {
+
+
 	public static void main(String[] args) {
 		System.out.println("Welcome to the Pin Code Problem");
-		
-		boolean matcher = "400088".matches("[\\d ]{6}");
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the PIN Code:");
+		String patternValidate = sc.nextLine();
+
+		boolean matcher = patternValidate.matches("^[\\d ]{6}");
 		if(matcher == true) {
 			System.out.println("Valid PIN Code");
 		}else {
--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # PIN-Code-Matching-Problem
 
-In this Use Case 1 we are matching the PIN Code with the given PinCode
+In Use Case 1 we are matching the PIN Code with the given PinCode
 if satisfies it gets validated else Invalid.
+
+In Use Case 2 we are matching the PIN code restricting the Alphabet at 
+the beggining of the pattern.
